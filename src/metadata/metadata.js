86cf165c1fc4434c402fe1c818c51371f98a2328
ananyajai <ananyajshankar@gmail.com>
2020-10-18 00:49:42 +0200

    More changes.

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -3,47 +3,14 @@ module.exports = {
   title: 'Mathematics and Economics student',
   baseUrl: 'https://sneas.github.io/cv/',
   facts: {
-    Residence: '<a href="https://goo.gl/maps/3JZUv4LVbG1bmtwN9"><i class="fa fa-home fact-icon"></i>Weesp, NL</a>',
+    Residence: '<a href="https://goo.gl/maps/MBMZsr8AkkRh4AHV9"><i class="fa fa-home fact-icon"></i>Durham, UK</a>',
     Email: '<a href="mailto:ananyajshankar@gmail.com">ananyajshankar@gmail.com</a>',
+    Phone: '+44-7387791518',
   },
   skills: [
     ['Python', '++++'],
     ['Matlab', '+++'],
     ['Kotlin', '++'],
     ['Latex', '+++'],
-  ],
-  positions: [
-    {
-      company: 'Durham University',
-      location: 'UK',
-      title: 'BSC. Mathematics & Economics',
-      period: 'Class of 2022',
-      skills: ['AWS', 'Terraform', 'Node', 'React', 'Mobx', 'Redux', 'Webpack', 'Jest'],
-      contents: `
-    Responsibilities:
-    * Incorporate business requirements into biweekly product increments
-      `,
-    },
-    {
-      company: 'upwork.com',
-      location: 'Mountain View, USA',
-      title: 'Senior Frontend Developer',
-      period: 'Oct 2012 – Jun 2014',
-      skills: ['Javascript', 'jQuery', 'qUnit', 'Grunt', 'Selenium', 'Compass/SASS', 'PHP', 'Zend', 'Postgres'],
-      contents: `
-Core frontend developer of the world's #1 freelance marketplace.
-Responsibilities:
-* Developed A/B tested functionality for company's crucial business component: Job Posting Engine
-* Conducted job interviews
-* Facilitated onboarding process
-Achievements:
-* Developed and successfully launched job post engine: highly-configured job posting and rendering tool
-* Introduced test driven development process into the organization's development flow
-`,
-      review: {
-        file: 'upwork.png',
-        author: 'Sean Kane, Vice President of Engineering',
-      }
-    },
   ],
 };
